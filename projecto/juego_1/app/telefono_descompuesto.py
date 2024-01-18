1d20e25a570a9a26771d77fb461295eebea26da2
Bysantosj17 <uriel_barbosasan@hotmail.com>
2024-01-18 01:15:03 -0600

    10/01/2024 1:00am

--- projecto/juego_1/app/telefono_descompuesto.py
+++ projecto/juego_1/app/telefono_descompuesto.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 from pygame.locals import *
 import sys
 
@@ -6,6 +6,9 @@ import sys
 pygame.init()
 
 #Crear ventana
+RED   = ( 255,   0,   0)
+BLUE  = (   0,   0, 255)
+
 w,h = 1000, 650
 PANTALLA = pygame.display.set_mode((w,h))
 
@@ -18,16 +21,49 @@ pygame.display.set_caption("Telefono descompuesto")
 icono = pygame.image.load('./juego_1/icono/icono_telefono.png')
 pygame.display.set_icon(icono)
 
+
 #numeros
 boxes = []
-for i in range(2):
-    x
+for i in range(5):
+    a = random.randint(40, 100)
+    b = random.randint(40, 100)
+    c = random.randint(40, 100)
+    d = random.randint(40, 100)
+    box = pygame.Rect(a, b, c, d)
+    boxes.append(box)
+    
+
 
 #Bucle del juego
-while True:
-    for event in pygame.event.get():
-        if event.type ==QUIT:
-            pygame.quit()
-            sys.exit()    
+run = True
+while run:
+    
+    for box in boxes:
+        pygame.draw.rect(PANTALLA, RED, box)
+    
+    for event in pygame.event.get():   
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                for num, box in enumerate(boxes):
+                    if box.collidepoint(event.pos):
+                        active_box = num
+                        
+
+        if event.type == pygame.MOUSEMOTION:
+            if active_box != None:
+                boxes[active_box].move_ip(event.rel)
+                
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 0:
+                    active_box = None
+                
+        if event.type == pygame.QUIT:
+            run = False         
+            
+    CUADRO = pygame.draw.rect(PANTALLA, BLUE, (400, 70, 550, 500))
+        
+        
     pygame.display.update()
+    
+pygame.quit()
 
