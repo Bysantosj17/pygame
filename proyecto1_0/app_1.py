a2d45884a525114b546e5b5b1e2b48d2b07553ae
Bysantosj17 <uriel_barbosasan@hotmail.com>
2024-02-16 19:22:17 -0600

    16/02/2024 7:22pm

--- proyecto1_0/app_1.py
+++ proyecto1_0/app_1.py
@@ -6,6 +6,7 @@ from menu.menu import *
 #Juego_1
 from juego1.app_juego1.telefono_sin_num import *
 
+
 if __name__ == "__main__":
     b = menu_game()
     b.corre_menu()
--- proyecto1_0/menu/menu.py
+++ proyecto1_0/menu/menu.py
@@ -1,6 +1,7 @@
 import pygame, sys,os
 
 from juego1.app_juego1.telefono_sin_num import *
+from menu.boton.boton_salir import * 
 
 pygame.init()
 
@@ -10,22 +11,39 @@ WHITE = (255,255,255)
 GREEN = (  0,255,  0)
 RED   = (255,  0,  0)
 BLUE  = (  0,  0,255)
+AZUL_CIELO = (92, 241, 254 )
 
 carpeta_proyecto = os.path.dirname(__file__)
 carpeta_img_menu = os.path.join(carpeta_proyecto, "img_menu")
-
-menu_img = pygame.image.load(os.path.join(carpeta_img_menu, "menu_img.jpg"))            
+    
 class menu_game:
     def __init__(self):
-        self.pantalla_menu = pygame.display.set_mode((800, 600))
-        pygame.display.set_caption("Menu Principal")
-        #Obtiene el rectangulo (sprite)
-        self.pantalla_menu.blit(menu_img, [ 0, 0])
-            
+        pygame.init()
+        self.ancho = 700
+        self.alto = 700
+        self.screen = pygame.display.set_mode((self.ancho, self.alto))
+        self.screen_width = self.screen.get_rect().width
+        self.screen_height = self.screen.get_rect().height
+        self.juego1_activado = False
+        self.color_font = (AZUL_CIELO)
+        self.btn_salir = Boton(self, "Play")
+        
+        
+
+    
     def corre_menu(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                    
+        
+            if not self.juego1_activado:
+                self.btn_salir.Dibuja_boton()
+                    
+            
+            self.screen.fill(self.color_font)
             pygame.display.flip()
             
+            
+    
